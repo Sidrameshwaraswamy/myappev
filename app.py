8121729c9e547b87adc70fc75e8026b64b1912c4
Sidrameshwaraswamy <118374544+Sidrameshwaraswamy@users.noreply.github.com>
2025-03-09 18:56:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,4 +96,4 @@ if __name__ == "__main__":
         db.create_all()
     
     # Ensure Flask listens on 0.0.0.0 inside Docker
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
