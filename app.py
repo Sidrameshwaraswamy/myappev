d48a92342bd80735a101cf1311315f0b469da508
Sidrameshwaraswamy <118374544+Sidrameshwaraswamy@users.noreply.github.com>
2025-03-09 19:53:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -90,10 +90,10 @@ def logout():
     logout_user()
     flash("You have been logged out.", "info")
     return redirect(url_for("home"))
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
     
     port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port or default to 5000
     app.run(host="0.0.0.0", port=port, debug=True)  # Bind to 0.0.0.0 for Render
-
